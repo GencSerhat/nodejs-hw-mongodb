6e2b62458e8876d0d80b9a8ffc5a586b3cefa5f9
Serhat GENÇ <gncsrht05@gmail.com>
2025-06-15 23:38:19 +0300

    test belge silindi. 404 hatası eklendi.

--- src/controllers/contactsController.js
+++ src/controllers/contactsController.js
@@ -1,25 +1,31 @@
-import { getAllContacts,getContactById } from "../services/contacts.js";
+import mongoose from 'mongoose';
+import { getAllContacts, getContactById } from '../services/contacts.js';
 
-export const getAllContactsController = async (req, res) =>{
-    try {
-        const contacts = await getAllContacts();
-        res.status(200).json({
-            status:200,
-            message:'Succesfully found contacts!',
-            data:contacts,
-        });
-    } catch (error) {
-        console.error('Kontrolde hata oluştu : ', error.message);
-        res.status(500).json({
-            status:500,
-            message:'Internal server error',
-        });
-    }
+export const getAllContactsController = async (req, res) => {
+  try {
+    const contacts = await getAllContacts();
+    res.status(200).json({
+      status: 200,
+      message: 'Succesfully found contacts!',
+      data: contacts,
+    });
+  } catch (error) {
+    console.error('Kontrolde hata oluştu : ', error.message);
+    res.status(500).json({
+      status: 500,
+      message: 'Internal server error',
+    });
+  }
 };
 
 export const getContactByIdController = async (req, res) => {
   try {
     const { contactId } = req.params;
+    
+     // ➤ ObjectId format kontrolü
+    if (!mongoose.Types.ObjectId.isValid(contactId)) {
+      return res.status(404).json({ message: 'Contact not found' });
+    }
 
     // Şu an burada getContactById fonksiyonunu çağıracağız (services klasöründen)
     const contact = await getContactById(contactId);
